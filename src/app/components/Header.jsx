1c8ec723104842ffedb0dffeb692acef935e1285
sagar <sagarkarotia@gmail.com>
2024-02-04 01:40:59 +0530

    link added

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -24,10 +24,10 @@ function Header() {
             <div className={!sidebar?'hidden':'absolute xl:hidden right-0 flex z-10'}>   
                 <CloseIcon onClick={()=>setSidebar(!sidebar)} fontSize='large' color='warning'/>
                 <ul className='uppercase h-screen w-64 p-10 space-y-5 bg-[#6991cc]'>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Properties</li>
-                    <li>Contact us</li>
+                <a href="#home"><li className='hover:text-yellow-500'>Home</li></a>
+                <a href="#about"><li className='hover:text-yellow-500'>About Us</li></a>
+                <a href="#property"><li className='hover:text-yellow-500'>Properties</li></a>
+                <a href="#contact"><li className='hover:text-yellow-500'>Contact us</li></a>
                 </ul>
             </div>
         </header>
